5055e4d985b7e15917e38267c9da293d39f5fc5f
Sam Minot <sam@fredhutch.org>
2019-06-05 13:48:55 -0700

    More generalized example

--- run.sh
+++ run.sh
@@ -8,6 +8,5 @@ nextflow \
     main.nf \
     --sample_sheet tests/sample_sheet.txt \
     --output_directory tests/output \
-    -work-dir s3://fh-pi-fredricks-d/lab/Sam_Minot/data/nextflow/work/ \
     -process.queue mixed \
     -resume
